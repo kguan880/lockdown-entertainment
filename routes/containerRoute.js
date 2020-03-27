8305be3076dd8e13904ba35e564c34b7c4d85784
Hieu Tran <tranhieunz@gmail.com>
2020-03-27 13:39:50 +1300

    Updated something

--- routes/containerRoute.js
+++ routes/containerRoute.js
@@ -25,99 +25,24 @@ router.get('/:id', (req, res) => {
 fs.readFile("./data.json", 'utf-8', (err,data) =>{
   if(err) throw err;
   let obj = JSON.parse(data)
-const id = req.params.id 
-const videoArray = data.video.find(item => item.id == id)
-
-
-const viewData = {
-  name: obj.name,
-  artist: obj.artist,
-  genre: obj.genre,
-  comment: obj.comment,
-  link: obj.link,
-  id: obj.id
-}
-}
-const template =''
-res.render(template, viewData)
-
-})
-
-
-
-/*potential refractoring idea
   
-router.get('/comedy/:id', (req, res)=>{
-  fs.readFile(comedyData, 'utf-8', (err,data)=>{
-    if(err) throw err;
-    let obj = JSON.parse(data)
-    console.log('comedy videos')
+  const id = req.params.id 
+  const videoArray = obj.video.find(item => item.id == id)
+  
+  const viewData = {
+    name: videoArray.name,
+    artist: videoArray.artist,
+    genre: videoArray.genre,
+    comment: videoArray.comment,
+    link: videoArray.link,
+    id: videoArray.id
+  }
 
-    const id = req.params.id
-    
-    const comedyVids = data.comedy.find(video => video.id === id)
+  const template ='./partials/video'
+  res.render(template, viewData)
 
-    const viewData = {
-      name: comedy.name,
-      artist: comedy.artist,
-      genre: comedy.genre,
-      comment: comedy.comment,
-      id: comedy.id
-    }
-
-    const template = ''
-    
-    res.render(template, viewData)
   })
 })
 
-router.get('/misc/:id', (req, res)=>{
-  fs.readFile(miscData, 'utf-8', (err,data)=>{
-    if(err) throw err;
-    let obj = JSON.parse(data)
-    console.log('misc videos')
-
-    const id = req.params.id
-    
-    const miscVids = data.misc.find(video => video.id === id)
-
-    const viewData = {
-      name: mics.name,
-      artist: misc.artist,
-      genre: misc.genre,
-      comment: misc.comment,
-      id: misc.id
-    }
-
-    const template = ''
-    
-    res.render(template, viewData)
-  })
-})
-
-router.get('/music/:id', (req, res)=>{
-  fs.readFile(musicData, 'utf-8', (err,data)=>{
-    if(err) throw err;
-    let obj = JSON.parse(data)
-    console.log('music videos')
-
-    const id = req.params.id
-    
-    const musicVids = data.music.find(video => video.id === id)
-
-    const viewData = {
-      name: music.name,
-      artist: music.artist,
-      genre: music.genre,
-      comment: music.comment,
-      id: music.id
-    }
-
-    const template = ''
-    
-    res.render(template, viewData)
-  })
-})
-*/
 
 module.exports = router;
